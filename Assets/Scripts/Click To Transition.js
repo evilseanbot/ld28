b17cb39434a23c2f52fab2f9ef8aec901e6ee616
Seanna Musgrave <evilseanbot@gmail.com>
2013-12-14 15:41:00 -0800

    Enabled clicking to move scene around.

---
+++ Assets/Scripts/Click To Transition.js
@@ -0,0 +1,20 @@
+﻿
+var mainCamera;
+
+function Awake() {
+    mainCamera = GameObject.FindGameObjectWithTag("MainCamera").transform;
+}
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function OnMouseDown() {
+    mainCamera.position.x = -3;
+    mainCamera.position.y = 2;
+    mainCamera.gameObject.camera.orthographicSize = 3;
+}
---
+++ Assets/Scripts/SmoothCameraMovement.js
@@ -0,0 +1,16 @@
+﻿var target = {"x": -1, "y": -1};
+var targetSize: float;
+function Start () {
+    
+}
+
+function Update () {
+    xDiff = target.x - transform.position.x;
+    yDiff = target.y - transform.position.y;
+    sizeDiff = targetSize - camera.orthographicSize;
+    
+    transform.position.x = transform.position.x + (xDiff/2);
+    transform.position.y = transform.position.y + (yDiff/2);
+    camera.orthographicSize = camera.orthographicSize + (sizeDiff/2);
+    
+}
