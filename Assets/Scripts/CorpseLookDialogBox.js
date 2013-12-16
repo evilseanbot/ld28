7c67d5179fac734ebcccd7a4168413c0c40b3865
Seanna Musgrave <evilseanbot@gmail.com>
2013-12-15 21:53:47 -0800

    Added dialog box

---
+++ Assets/Scripts/CorpseLookDialogBox.js
@@ -0,0 +1,7 @@
+﻿function Update() {
+    GameObject.Find("DialogText").GetComponent("TextMesh").text = "Well, I guess I always wanted to look at a dead body. \n it isn't the same when you're a ghost, though, no spine to \ntingle.";        
+}
+
+function OnMouseDown() {
+    Destroy(gameObject);
+}
